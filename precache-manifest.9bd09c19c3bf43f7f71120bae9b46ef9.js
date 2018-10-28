f37061c6bba658818ab0522b71cd50d99956befc
Cian O'Gralaigh <ihatecian@gmail.com>
2018-10-28 15:17:31 +0000

    Updates

---
+++ precache-manifest.9bd09c19c3bf43f7f71120bae9b46ef9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42f14dc63953a72b0946",
+    "url": "/card-matching-game-react/static/js/runtime~main.42f14dc6.js"
+  },
+  {
+    "revision": "53739a1aed3082b4409b",
+    "url": "/card-matching-game-react/static/js/main.53739a1a.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/card-matching-game-react/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "53739a1aed3082b4409b",
+    "url": "/card-matching-game-react/static/css/main.30497286.chunk.css"
+  },
+  {
+    "revision": "e8430c771ce51924843e8031295cbd91",
+    "url": "/card-matching-game-react/index.html"
+  }
+];
